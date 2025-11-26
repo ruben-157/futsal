e3852aee8f9dc0eb15fc493e5019028db9b9da7c
ruben-157 <rubenvdkamp@gmail.com>
2025-11-26 12:59:37 +0100

    new update

--- scripts/main.js
+++ scripts/main.js
@@ -840,7 +840,7 @@ async function populateMvpChaseCard(refs){
       colgroup.appendChild(col);
     });
     const thead = document.createElement('thead');
-    thead.innerHTML = '<tr><th>Player</th><th><span class="label-full">Current PTS/S</span><span class="label-short">PTS/S</span></th><th><span class="label-full">PTS Needed for MVP</span><span class="label-short">PTS Needed</span></th></tr>';
+    thead.innerHTML = '<tr><th>Player</th><th><span class="label-full">Current Points per Session</span><span class="label-short">PTS/S</span></th><th><span class="label-full">Points needed for MVP Badge</span><span class="label-short">PTS Needed</span></th></tr>';
     const tbody = document.createElement('tbody');
     const toShow = result.rows.slice(0,5);
     for(const row of toShow){
